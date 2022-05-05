a0b50df9c2a8f71e972d8f316d7e5df8a8e9cef3
Roberto Calestini <betocalestini@hotmail.com>
2022-05-05 10:26:46 -0300

    another commit

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"api_rest_go_01/models"
 	"api_rest_go_01/routes"
-	env "api_rest_go_01/services"
 	"fmt"
 )
 
 func main() {
-	env.Load()
+
 	models.Personalidades = []models.Personalidade{
 		{ID: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{ID: 2, Nome: "Nome 1", Historia: "Historia 1"},
